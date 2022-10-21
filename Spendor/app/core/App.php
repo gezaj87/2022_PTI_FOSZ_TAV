9ab06ace7329a5f5b1ddbe4533a523535f980086
J. Géza <gezajoni87@gmail.com>
2022-10-21 09:59:18 +0200

    #77 Router.php, App.php

--- Spendor/app/core/App.php
+++ Spendor/app/core/App.php
@@ -2,5 +2,25 @@
 
 class App
 {
-    
+    private array $url;
+
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) session_start();
+
+        $url = '/'.$_GET['url'];
+        $this->url = explode('/', $url);
+
+        $Router = new Router($this->url);
+
+        //útvonalak definiálása ezen sor altt
+        
+        
+
+        if (!isset($_SESSION['router']) || !$_SESSION['router']) {
+            http_response_code(404);
+        } else {
+            $_SESSION['router'] = false;
+        }
+    }
 }
