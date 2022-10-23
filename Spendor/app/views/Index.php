7338a6d48a5364604382b709dae928c3c7e3b884
norosz <kulonnor@gmail.com>
2022-10-23 20:05:50 +0200

    #73
    img src javítás

--- Spendor/app/views/Index.php
+++ Spendor/app/views/Index.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Spendor-Bejelentkezés</title>
-        <link rel="stylesheet" href="/Spendor/Spendor/public/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/Spendor/Spendor/public/css/style.css">
-        <script src="/Spendor/Spendor/public/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="Spendor/public/css/bootstrap.min.css">
+        <link rel="stylesheet" href="Spendor/public/css/style.css">
+        <script src="Spendor/public/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     </head>
     <body>
@@ -15,7 +15,7 @@
             <div class="row">                
                 <h1>
                     <a>
-                        <img src="/Spendor/public/img/logo4.jpg" alt="logo" height="70">
+                        <img src="public/img/logo4.jpg" alt="logo" height="70">
                     </a>
                     PTI-FOSZ-TAV-2022-AFP
                 </h1>                
