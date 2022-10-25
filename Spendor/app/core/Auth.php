1bf71029f5a95e72e7bb858e462fe8860a850097
J. Géza <gezajoni87@gmail.com>
2022-10-25 14:23:16 +0200

    #96 Auth.php

---
+++ Spendor/app/core/Auth.php
@@ -0,0 +1,15 @@
+<?php
+
+class Auth
+{
+    public function Auth(bool $admin = false)
+    {
+        if (!isset($_SESSION['token']) || !isset($_SESSION['email'])) return false;
+
+        $result = Database::SQL('select * from felhasznalok where email = ? and admin = ?', [$_SESSION['email'], $admin? 1 : 0 ]) -> fetch_assoc();
+
+        if (!$result) return false;
+
+        return true;
+    }
+}
