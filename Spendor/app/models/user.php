713a9b70246a4c1ff5cc70fcfdddd14b0d905487
Gábor Polgár <gabor.polgar@gmail.com>
2022-11-03 20:35:44 +0100

    final fixing.
    fixed the user model code.

--- Spendor/app/models/user.php
+++ Spendor/app/models/user.php
@@ -1,13 +1,12 @@
 <?php
 
 class User {
-    private int $id;
+    private int $id = null;
     private string $password;
     private string $email;
     private string $name;
     private DateTime $birth_of_date;
     private ?string $phone_number = null;
-    private bool $admin;
 
     private const INVALID_ = "A megnevezés 1 és ";
     private const _TEXT = " karakter között lehet!";
@@ -16,25 +15,24 @@ class User {
     private const MAX_LENGTH_OF_NAME = 100;
     private const MAX_LENGTH_OF_PHONE_NUMBER = 15;
     private const MIN_LENGTH_OF_PASSW = 4;
+    private const INVALID_EMAIL = "Nem megfelelő email formátum. A karaktereinek 1 és 60 közé kell esnie!";
 
-    public function __construct(int $id, string $password, string $email, string $name, DateTime $birth_of_date, bool $admin, string $phone_number = null){
+    public function __construct(int $id=null, string $password, string $email, string $name, DateTime $birth_of_date, string $phone_number = null){
         if ($id)
         {
             if (self::PositiveIntegerCheck($id)) $this->id = $id;
             else throw new Exception(self::NOT_POSITIVE_INTEGER);
         }
         if (self::StringCheck($password, self::MAX_LENGTH_OF_PASSW_EMAIL, self::MIN_LENGTH_OF_PASSW)) $this->password = $password;
-        else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_PASSW_EMAIL.self::_TEXT);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) throw new Exception("Invalid email format");
+        else throw new Exception("A megnevezés 4 és ".self::MAX_LENGTH_OF_PASSW_EMAIL.self::_TEXT);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL) || (self::StringCheck($email, self::MAX_LENGTH_OF_PASSW_EMAIL))) throw new Exception(self::INVALID_EMAIL);
         $this->email = $email;
-        //if (self::StringCheck($email, self::MAX_LENGTH_OF_PASSW_EMAIL)) $this->email = $email;
-        //else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_PASSW_EMAIL.self::_TEXT);
         if (self::StringCheck($name, self::MAX_LENGTH_OF_NAME)) $this->name = $name;
         else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_NAME.self::_TEXT);
         if (self::StringCheck($phone_number, self::MAX_LENGTH_OF_PHONE_NUMBER)) $this->phone_number = $phone_number;
         else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_PHONE_NUMBER.self::_TEXT);
         $this->birth_of_date =$birth_of_date;
-        $this->admin = $admin;
+
     }
 
     public function getId(): int
@@ -65,11 +63,6 @@ class User {
     public function getPhone_number(): string
     {
         return $this->phone_number;
-    }
-    
-    public function getAdmin(): bool
-    {
-        return $this->bool;
     }
 
     private static function StringCheck(string $text, int $max_of_length, int $min_of_length=0): bool
