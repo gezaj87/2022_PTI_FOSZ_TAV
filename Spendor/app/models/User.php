5d614823005c188010b07efcb34bb0ca7f34d146
J. Géza <gezajoni87@gmail.com>
2022-11-04 00:41:21 +0100

    #79
    User validáció javítás, kiegészítés

--- Spendor/app/models/User.php
+++ Spendor/app/models/User.php
@@ -8,15 +8,17 @@ class User {
     private DateTime $birth_of_date;
     private ?string $phone_number = null;
 
-    private const INVALID_ = "A megnevezés 1 és ";
+    private const INVALID_ = "A név 1 és ";
     private const _TEXT = " karakter között lehet!";
     private const NOT_POSITIVE_INTEGER = "A megadott érték nem pozitív egész szám!";
     private const MAX_LENGTH_OF_PASSW_EMAIL = 60;
     private const MAX_LENGTH_OF_NAME = 100;
     private const MAX_LENGTH_OF_PHONE_NUMBER = 15;
     private const MIN_LENGTH_OF_PASSW = 4;
-    private const INVALID_PASS="A megnevezés 4 és ";
+    private const INVALID_PASS="A jelszó 4 és ";
     private const INVALID_EMAIL = "Nem megfelelő email formátum. A karaktereinek 1 és 60 közé kell esnie!";
+
+    private const INVALID_DATE_1 = "Helytelen születési dátum!";
 
     public function __construct(string $password, string $email, string $name, DateTime $birth_of_date, string $phone_number = null, int $id=null){
         if ($id)
@@ -30,9 +32,19 @@ class User {
         $this->email = $email;
         if (self::StringCheck($name, self::MAX_LENGTH_OF_NAME)) $this->name = $name;
         else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_NAME.self::_TEXT);
-        if ($phone_number && self::StringCheck($phone_number, self::MAX_LENGTH_OF_PHONE_NUMBER)) $this->phone_number = $phone_number;
-        else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_PHONE_NUMBER.self::_TEXT);
-        $this->birth_of_date =$birth_of_date;
+
+        if ($phone_number)
+        {
+            if (self::StringCheck($phone_number, self::MAX_LENGTH_OF_PHONE_NUMBER))
+            {
+                $this->phone_number = $phone_number;
+            }
+            else throw new Exception(self::INVALID_.self::MAX_LENGTH_OF_PHONE_NUMBER.self::_TEXT);
+        }
+
+        if (self::DateCheck($birth_of_date)) $this->birth_of_date =$birth_of_date;
+        else throw new Exception(self::INVALID_DATE_1);
+        
 
     }
 
@@ -69,7 +81,7 @@ class User {
     private static function StringCheck(string $text, int $max_of_length, int $min_of_length=0): bool
     {
         $text_length = strlen($text);
-        if ($text_length > $min_of_length && $text_length <= $max_of_length)
+        if ($text_length >= $min_of_length && $text_length <= $max_of_length)
         {
             return true;
         }
@@ -89,6 +101,15 @@ class User {
         {
             return false;
         }
+    }
+
+    private static function DateCheck($date_input)
+    {
+        $date_now = date_create("now");
+        $date_120years = date_sub(date_create("now"), date_interval_create_from_date_string("120 years"));
+
+        if ($date_now < $date_input || $date_input < $date_120years) return false;
+        else return true;
     }
 
 
