9f2869f1db88006b9cd0d278c57923ec352418fc
J. Géza <gezajoni87@gmail.com>
2022-11-06 12:21:49 +0100

    #98 javítás

--- Spendor/app/views/Register.php
+++ Spendor/app/views/Register.php
@@ -1,10 +1,16 @@
 <h3>Register.php view</h3>
 <?php 
-    if ($data['data']['validation_success'] && $data['data']['database_success'])
+
+    if (isset($data['data']['validation_success']))
     {
-        echo "Sikeres regisztráció!";
+        if ($data['data']['validation_success'] && $data['data']['database_success'])
+        {
+            echo "Sikeres regisztráció!";
+        }
+        else echo($data['data']['message']);
     }
-    else echo($data['data']['message']);
+
+    
 ?>
 <div class="container">
     <div class="row">
@@ -12,7 +18,7 @@
             <div class="content">
                 <form class="regisztracio" method="POST" action="register">
                     <label class="regisztracioCimke" for="regisztracioNev_id">Név</label>
-                    <input type="text" name="felhasznalonev" id="regisztracioNev_id" name="name" required><br>
+                    <input type="text" id="regisztracioNev_id" name="name" required><br>
                     <label class="regisztracioCimke" for="regisztracioEmail_id">E-mail cím</label>
                     <input type="email" id="regisztracioEmail_id" name="email" required><br>
                     <label class="regisztracioCimke" for="regisztracioJelszo_id">Jelszó</label>
@@ -20,9 +26,9 @@
                     <label class="regisztracioCimke" for="regisztracioJelszoUjra_id">Jelszó újra</label>
                     <input type="password" id="regisztracioJelszoUjra_id" name="password2" required><br>
                     <label class="regisztracioCimke" for="regisztracioSzulDatum_id">Születési dátum</label>
-                    <input type="date" name="regisztracioSzulDatum" id="regisztracioSzulDatum_id" name="date" required><br>                         
+                    <input type="date" id="regisztracioSzulDatum_id" name="date" required><br>                         
                     <label class="regisztracioCimke" for="regisztracioTel_id">Telefonszám</label>
-                    <input type="tel" id="regisztracioTel_id" pattern="[0]{1}[0-9]{10}" placeholder="201234567" name="phone" required><br>
+                    <input type="tel" id="regisztracioTel_id" pattern="[0]{1}[0-9]{10}" placeholder="201234567" name="phone"><br>
                     <input type="submit" class="regisztracioGombok" id="regRegisztralas" value="Regisztráció">
                     <input type="button" class="regisztracioGombok" id="megse" value="Mégse">
                  </form>
